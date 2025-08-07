7049c9290fee8578c507bdca58d4c3bf33e16a87
AnnaPiachenina <annapiachenina@gmail.com>
2025-08-07 13:37:43 +0200

    fetch data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useState } from 'react';
 import './styles/style.css'   
 const Weather = () => {
+
+    const [weatherData, setWeatherData] = useState(false);
+
+    const search = async (name) => {
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+
+            setWeatherData({
+                location: data.name,
+                temperature: Math.round(data.main.temp),
+                feels_like: Math.round(data.main.feels_like),
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                icon: data.weather[0].icon,
+                wind: data.wind.speed,
+
+            });
+        }   catch (error) {
+
+        }
+    }
+
+    useEffect(() => {
+        search ('Prague');
+    },[])
+
   return (
     <div className='weather-card'>
         <div className='main-info'>
+            <div>
+                <input type="text" placeholder='search' className='search-bar'/>
+            </div>
             <h1>Mesto</h1>
+            <img
+                src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
+                alt={weatherData.description}
+                />
             <h2>characteristika</h2>
             <p>26</p>
         </div>
